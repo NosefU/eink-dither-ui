b6b85bbec00d9f56b11c8eb6a78c03cf9aaff914
Dmitry Valko <dvalko@ptsecurity.com>
2025-02-08 20:42:27 +0300

    feat: add cell size

--- static/dither.js
+++ static/dither.js
@@ -7,9 +7,10 @@ const ditherjs = new DitherJS();
 
 function applyDither(imageData) {
   const algorithm = algorithmSelect.value;
+  const cellSize = Number(cellSizeInput.value || Math.floor(Math.max(1, cellSizeInput.min)));
 
   ditherjs.ditherImageData(imageData, {
-    "step": 1,
+    "step": cellSize,
     "algorithm": algorithm,  // Используем выбранный алгоритм
     "palette": palette,
   });
--- static/image-processing.js
+++ static/image-processing.js
@@ -2,15 +2,18 @@ const imageInput = document.getElementById('imageInput');
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const algorithmSelect = document.getElementById('algorithmSelect');
+const cellSizeInput = document.getElementById('cellSize');
 const shadowSlider = document.getElementById('shadowSlider');
 const midtoneSlider = document.getElementById('midtoneSlider');
 const highlightSlider = document.getElementById('highlightSlider');
 
 window.originalImageData = null;
 
+
 imageInput.addEventListener('change', handleImageUpload);
 algorithmSelect.addEventListener('change', handleAlgorithmChange);
 shadowSlider.addEventListener('input', updateImage);
+cellSizeInput.addEventListener('input', handleCellSizeChange);
 midtoneSlider.addEventListener('input', updateImage);
 highlightSlider.addEventListener('input', updateImage);
 
@@ -60,6 +63,26 @@ function handleAlgorithmChange() {
   applyDither(imageData);
 
   ctx.putImageData(imageData, 0, 0);
+}
+
+function handleCellSizeChange() {
+  if (!window.originalImageData) return;
+
+  if (cellSizeInput.value === "") return;
+
+  let value = Math.floor(Number(cellSizeInput.value) || 1);
+
+  if (value < Math.max(1, cellSizeInput.min)) {
+    value = Math.floor(Math.max(1, cellSizeInput.min));
+  } else if (value > cellSizeInput.max) {
+    value = Math.floor(cellSizeInput.max);
+  } else {
+    value = Math.floor(cellSizeInput.value);
+  }
+
+  cellSizeInput.value = value;
+
+  updateImage();
 }
 
 function updateImage() {
