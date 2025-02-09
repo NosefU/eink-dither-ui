c8dc68c5bd6fa2d9f4470de641cc333953da7cb0
Dmitry Valko <dvalko@ptsecurity.com>
2025-02-09 18:31:08 +0300

    feat: add don't dither algorithm

--- static/dither.js
+++ static/dither.js
@@ -9,6 +9,8 @@ const palettes = {
 
 function applyDither(imageData) {
   const algorithm = algorithmSelect.value;
+  if (algorithm === "null") return reducePalette(imageData);
+
   const cellSize = Number(cellSizeInput.value || Math.floor(Math.max(1, cellSizeInput.min)));
   const palette = palettes[paletteSelect.value];
 
@@ -17,4 +19,37 @@ function applyDither(imageData) {
     "algorithm": algorithm,  // Используем выбранный алгоритм
     "palette": palette,
   });
+}
+
+
+function reducePalette(imageData) {
+  const palette = palettes[paletteSelect.value];
+  const data = imageData.data;
+
+  // Функция для нахождения ближайшего цвета в заданной палитре
+  function findClosestColor(color, palette) {
+    let minDist = Infinity;
+    let closestColor = palette[0];
+
+    for (let i = 0; i < palette.length; i++) {
+      let dist = Math.pow(color[0] - palette[i][0], 2) +
+          Math.pow(color[1] - palette[i][1], 2) +
+          Math.pow(color[2] - palette[i][2], 2);
+      if (dist < minDist) {
+        minDist = dist;
+        closestColor = palette[i];
+      }
+    }
+    return closestColor;
+  }
+
+  // Применяем новую палитру к изображению
+  for (let i = 0; i < data.length; i += 4) {
+    let newColor = findClosestColor([data[i], data[i + 1], data[i + 2]], palette);
+    data[i] = newColor[0];
+    data[i + 1] = newColor[1];
+    data[i + 2] = newColor[2];
+  }
+
+  ctx.putImageData(imageData, 0, 0);
 }
